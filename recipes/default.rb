2b45d8e984391ffa0d7f1b4cffd013e6071a8bdf
Eric Marthinsen <eric@ericmarthinsen.com>
2016-03-12 01:47:20 -0500

    Include autostart file

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,8 +4,16 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-include_recipe "apt"
+include_recipe 'apt'
 
 package 'onboard' do
   action :install
+end
+
+cookbook_file '/etc/xdg/autostart/onboard.desktop' do
+  action :create
+  source 'onboard.desktop'
+  owner  'root'
+  group  'root'
+  mode   '0644'
 end
