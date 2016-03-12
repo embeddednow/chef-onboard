100a5becee5116832eb47a4ef8caec4750f12c75
Eric Marthinsen <eric@ericmarthinsen.com>
2016-03-12 00:51:56 -0500

    Add code to install onboard

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name 'onboard'
 maintainer 'Embedded Now, Inc'
-maintainer_email 'you@example.com'
+maintainer_email 'hello@embeddednow.com'
 license 'all_rights'
 description 'Installs/Configures onboard'
 long_description 'Installs/Configures onboard'
 version '0.1.0'
+
+depends "apt"
